826ca5197367bbfbd03ee872751f7cbc458d64e1
George Small <geomall92@gmail.com>
2020-02-08 16:43:31 -0500

    Combined F1 and F3 GPIO init code, again

--- libcanard_example/can.c
+++ libcanard_example/can.c
@@ -28,15 +28,20 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
 {
 
   GPIO_InitTypeDef GPIO_InitStruct;
+
   if (canHandle->Instance==CAN1) {
 
-    /* CAN1 clock enable */
+    // GPIO Ports Clock Enable
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    // CAN1 clock enable
     __HAL_RCC_CAN1_CLK_ENABLE();
   
-    /**CAN GPIO Configuration    
-    PA11     ------> CAN_RX
-    PA12     ------> CAN_TX 
-    */
+    // CAN GPIO Configuration    
+    // PA11     ------> CAN_RX
+    // PA12     ------> CAN_TX 
+
+#if defined (STM32F103x6) || defined (STM32F103xB)
     GPIO_InitStruct.Pin = GPIO_PIN_11;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -46,11 +51,22 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+#elif defined (STM32F303x8) || defined (STM32F303xC) || defined (STM32F303xE)
+    GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF9_CAN;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+#else
+#error "Warning untested processor variant"
+#endif
 
   }
 
 }
 
+/*
 
 void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
 {
@@ -66,3 +82,5 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
   }
 
 }
+
+*/
