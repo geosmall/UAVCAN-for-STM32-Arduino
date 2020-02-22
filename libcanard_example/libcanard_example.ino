39e59308b007c6614d132f6fa9f6f73cd9888778
George Small <geomall92@gmail.com>
2020-02-22 09:55:55 -0500

    Branch for AP_Periph mods

--- libcanard_example/libcanard_example.ino
+++ libcanard_example/libcanard_example.ino
@@ -3,35 +3,35 @@
 #define RC_NUM_CHANNELS_TO_PRINT    4
 
 extern "C" void printInitMsg(void) {
-  Serial.begin(115200);
-  Serial.print("Initializing...");
-  Serial.print(HAL_RCC_GetHCLKFreq());
-  Serial.println();
+  Serial1.begin(115200);
+  Serial1.print("Initializing...");
+  Serial1.print(HAL_RCC_GetHCLKFreq());
+  Serial1.println();
 }
 
 extern "C" void printArray(int16_t arr[]) {
 
   char str[20];
   itoa(arr[0], str, 10);
-  Serial.print("ESC Array: ");
-  Serial.print(str);
+  Serial1.print("ESC Array: ");
+  Serial1.print(str);
   for (int i = 1; i < RC_NUM_CHANNELS_TO_PRINT; i++) {
     itoa(arr[i], str, 10);
-    Serial.print(", ");
-    Serial.print(str);
+    Serial1.print(", ");
+    Serial1.print(str);
   }
-  Serial.println();
+  Serial1.println();
 }
 
 extern "C" void println(char* str) {
-  Serial.print(str);
-  Serial.println();
+  Serial1.print(str);
+  Serial1.println();
 }
 
 extern "C" void printstr(char* str) {
-  Serial.print(str);
+  Serial1.print(str);
 }
 
 extern "C" void printint(uint32_t num) {
-  Serial.print(num, HEX);
+  Serial1.print(num, HEX);
 }
--- libcanard_example/main.c
+++ libcanard_example/main.c
@@ -1,14 +1,77 @@
 #include "Arduino.h"
 #include "stm32yyxx_ll.h"
+#include "app_comms.h"
 
+#define FW_MAJOR 1
+#define FW_MINOR 0
+#define APJ_BOARD_ID 1000
+
+#define HAL_RAM0_START 0x20000000
+#define HAL_RAM_RESERVE_START 0x00000100
+
+/* Private variables ---------------------------------------------------------*/
+const struct app_descriptor app_descriptor __attribute__((section(".app_descriptor"))) = {
+  { 0x40, 0xa2, 0xe4, 0xf1, 0x64, 0x68, 0x91, 0x06 },   // uint8_t sig[8]
+  0x1234,         //  uint32_t image_crc1 is the crc32 from firmware start to start of image_crc1
+  0xabcd,         //  uint32_t image_crc2 is the crc32 from the start of version_major to the end of the firmware
+  0x0000,         //  uint32_t image_size total size of firmware image in bytes
+  0x0000,         //  uint32_t git hash
+  FW_MAJOR,       //  uint8_t  software version MAJOR
+  FW_MINOR,       //  uint8_t  software version MINOR
+  APJ_BOARD_ID,   //  uint16_t board_id = APJ_BOARD_ID
+  { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },   // uint8_t reserved[8]
+};
 
 const int PwmOutputPin = PA0;         // PA_0,D46/A0 -- USES TIM2
 const int TestOutputPin = PA6;
 
 unsigned long previousMillis = 0;     // stores last time output was updated
-const long interval = 1000;           // interval at which to print output (milliseconds)
+const long interval = 500;            // interval at which to print output (milliseconds)
+
+
+typedef struct
+{
+  __IO uint32_t KR;   /*!< IWDG Key register,       Address offset: 0x00 */
+  __IO uint32_t PR;   /*!< IWDG Prescaler register, Address offset: 0x04 */
+  __IO uint32_t RLR;  /*!< IWDG Reload register,    Address offset: 0x08 */
+  __IO uint32_t SR;   /*!< IWDG Status register,    Address offset: 0x0C */
+  __IO uint32_t WINR; /*!< IWDG Window register,    Address offset: 0x10 */
+} IWDG_Regs;
+
+#define IWDGD (*(IWDG_Regs *)(IWDG_BASE))
+
+static bool watchdog_enabled;
+
+/*
+  setup the watchdog
+ */
+void stm32_watchdog_init(void)
+{
+    // setup for 2s reset
+    IWDGD.KR = 0x5555;
+    IWDGD.PR = 2; // div16
+    IWDGD.RLR = 0xFFF;
+    IWDGD.KR = 0xCCCC;
+    watchdog_enabled = true;
+}
+
+/*
+  pat the dog, to prevent a reset. If not called for 1s
+  after stm32_watchdog_init() then MCU will reset
+ */
+void stm32_watchdog_pat(void)
+{
+    if (watchdog_enabled) {
+        IWDGD.KR = 0xAAAA;
+    }
+}
+
 
 void setup() {
+
+  struct app_bootloader_comms *comms = (struct app_bootloader_comms *)HAL_RAM0_START;
+  memset(comms, 0, sizeof(struct app_bootloader_comms));
+  comms->magic = APP_BOOTLOADER_COMMS_MAGIC;
 
   printInitMsg();
   println();
@@ -18,13 +81,14 @@ void setup() {
   pinMode(PwmOutputPin, OUTPUT);
   pinMode(TestOutputPin, OUTPUT);
 
-  digitalWrite(LED_BUILTIN, HIGH);
-
-//  uint32_t ret = DWT_Delay_Init();
-
   CAN_HW_Init();
 
   uavcanInit();
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+  //stm32_watchdog_init();
+  stm32_watchdog_pat();
 
 }
 
@@ -33,6 +97,8 @@ void loop() {
   unsigned long currentMillis;
 
   currentMillis = millis();
+
+  stm32_watchdog_pat();
 
   sendCanard();
   receiveCanard();
